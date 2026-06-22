04feb2864265fbef232b63bed9c9741f2ff1ae4e
TimLxH <33raymundoc@gmail.com>
2026-06-22 08:55:56 -0500

    fit:correc

--- client/src/pages/admin/Pedidos.jsx
+++ client/src/pages/admin/Pedidos.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useToast } from '../../components/Toast';
 import { ClipboardList, Clock, CheckCircle2, Ban, Play, Check, CheckSquare } from 'lucide-react';
-import { supabase } from "../../supabase";
+import { supabase } from "../../context/AppContext";
 
 export default function Pedidos() {
   const [pedidos, setPedidos] = useState([]);
